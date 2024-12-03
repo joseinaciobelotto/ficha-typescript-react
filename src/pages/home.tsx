ce57338d8e71024d005f32377a92d208aeeaf89d
vitor hugo de abreu schell <vitorlegalhugi@gmail.com>
2024-12-03 00:37:46 -0300

    Backup

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,89 +1,92 @@
-import { useState, useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Card, CardContent, CardFooter } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
-import { Star, ShoppingBag, User } from 'lucide-react';
-import axios from 'axios';
-import { Link } from 'react-router-dom';
+import { User, Mail, DollarSign } from 'lucide-react';
 
-// Definindo a interface para o tipo de restaurante
-interface Restaurant {
-    id: number;
-    name: string;
-    description: string;
-    image: string;
-    rating: number;
+interface User {
+  id: string;
+  name: string;
+  email: string;
+  saldo: string;
 }
 
-export default function Component() {
-    const [restaurants, setRestaurants] = useState<Restaurant[]>([]);
+export default function Profile() {
+  const [user, setUser] = useState<User | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        const fetchRestaurants = async () => {
-            try {
-                const response = await axios.get<Restaurant[]>('https://apifakedelivery.vercel.app/restaurants');
-                setRestaurants(response.data);
-            } catch (err) {
-                console.error(err);
-            }
-        };
+  useEffect(() => {
+    const fetchUser = async () => {
+      const userId = localStorage.getItem('userId');
+      if (!userId) {
+        setError('Usuário não autenticado.');
+        setLoading(false);
+        return;
+      }
 
-        fetchRestaurants();
-    }, []);
+      try {
+        const response = await fetch(`https://apifakedelivery.vercel.app/users/${userId}`);
+        if (!response.ok) {
+          throw new Error('Erro ao buscar os dados do usuário');
+        }
+        const data: User = await response.json(); // Tipando a resposta
+        setUser(data);
+      } catch (error: any) { // Capturando o erro como qualquer tipo
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
 
-    return (
-        <div className="min-h-screen bg-gray-100">
-            <header className="bg-black shadow">
-                <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-6">
-                    <div className="flex justify-between items-center">
-                        <div className="flex items-center">
-                            <ShoppingBag className="h-8 w-8 text-primary mr-2 text-white" />
-                            <span className="font-bold text-xl text-white">Cimarmitas</span>
-                        </div>
-                        <div className="flex items-center justify-center w-10 h-10 rounded-full bg-white text-black shadow-md">
-                            <Link to={"/profile"}>
-                                <User className="h-5 w-5" />
-                            </Link>
-                        </div>
-                    </div>
-                </div>
-            </header>
+    fetchUser();
+  }, []);
 
-            <div className="bg-gradient-to-r from-primary to-primary-foreground text-white">
-                <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12 text-center">
-                    <h1 className="text-4xl font-bold mb-4 text-black">Bem vindo ao Cimarmitas</h1>
-                    <p className="text-xl text-black">Seus restaurantes favoritos na porta da sua casa</p>
-                </div>
+  const handleLogout = () => {
+    localStorage.removeItem('userId'); // Remove o ID do usuário
+    navigate('/'); // Redireciona para a página de login
+  };
+  if (loading) {
+    return <div className="text-center py-6">Carregando...</div>;
+  }
+
+  if (error) {
+    return <div className="text-center py-6 text-red-500">{error}</div>;
+  }
+
+  if (!user) {
+    return <div className="text-center py-6 text-gray-600">Nenhum usuário encontrado.</div>;
+  }
+
+  return (
+    <div className="min-h-screen bg-gray-100 py-6">
+      <div className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8">
+        <h1 className="text-3xl font-bold mb-8 text-center">Perfil do Usuário</h1>
+        <Card className="mb-6">
+          <CardContent>
+            <div className="flex items-center mb-4 mt-6">
+              <User className="h-10 w-10 text-primary mr-3" />
+              <h2 className="text-2xl font-bold">{user.name}</h2>
             </div>
-
-            <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
-
-                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-                    {restaurants.map(restaurant => (
-                        <Card key={restaurant.id} className="overflow-hidden">
-                            <div className="relative">
-                                <img src={restaurant.image} alt={restaurant.name} className="w-full h-48 object-cover" />
-                            </div>
-                            <CardHeader>
-                                <CardTitle>{restaurant.name}</CardTitle>
-                            </CardHeader>
-                            <CardContent>
-                                <p className="text-gray-600">{restaurant.description}</p>
-                                <div className="flex items-center mt-2">
-                                    <Star className="h-5 w-5 text-yellow-400 mr-1" />
-                                    <span>{restaurant.rating}</span>
-                                </div>
-                            </CardContent>
-                            <CardFooter className="flex justify-between mt-6">
-                                <span className="text-gray-600"></span>
-                                <Link to={`/restaurant/${restaurant.id}`}>
-                                    <Button>Ver restaurante</Button>
-                                </Link>
-
-                            </CardFooter>
-                        </Card>
-                    ))}
-                </div>
-            </main>
-        </div>
-    );
+            <div className="flex items-center mb-2">
+              <Mail className="h-5 w-5 text-gray-600 mr-2" />
+              <span className="text-lg">{user.email}</span>
+            </div>
+            <div className="flex items-center">
+              <DollarSign className="h-5 w-5 text-green-600 mr-2" />
+              <span className="text-lg">Saldo: R$ {user.saldo}</span>
+            </div>
+          </CardContent>
+          <CardFooter className="flex justify-end">
+            <Button variant="outline" className="mr-2">Editar</Button>
+            <Button variant="destructive">Excluir Conta</Button>
+            <Button variant="destructive" onClick={handleLogout}>
+              Logout
+            </Button>
+          </CardFooter>
+        </Card>
+      </div>
+    </div>
+  );
 }
